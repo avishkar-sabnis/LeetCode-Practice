4828d4c5e9d194a35ce27678161b5f30bc6e6041
Avishkar Sabnis <32098673+avishkar-sabnis@users.noreply.github.com>
2026-04-18 18:28:33 +0530

    RomanToInteger

---
+++ SubstringIndex.java
@@ -0,0 +1,22 @@
+public class SubstringIndex {
+
+    public static void main(String[] args) {
+
+        String haystack = "sadbutsad";
+        String needle = "sawd";
+
+        int index = haystack.indexOf(needle);
+        System.out.println(index);
+
+
+
+
+
+
+
+
+
+
+    }
+
+}
